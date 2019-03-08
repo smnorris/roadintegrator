12f01939457591089f3dd2a5ac7a164a1d42f00d
Simon Norris <snorris@hillcrestgeo.ca>
2019-03-08 14:55:47 -0800

    merge tiles into postgres rather than .gdb - much speedier and we can more easily manipulate the results

--- 3_merge.py
+++ 3_merge.py
@@ -1,13 +1,14 @@
 import os
 import csv
+from subprocess import Popen, PIPE
 
 import shutil
 import subprocess
 import yaml
 import click
-
 import fiona
 
+import pgdata
 
 # just hold all tile names as a string to make things simple
 TILES = "082E,082F,082G,082J,082K,082L,082M,082N,082O,083C,083D,083E,092B,092C,092E,092F,092G,092H,092I,092J,092K,092L,092M,092N,092O,092P,093A,093B,093C,093D,093E,093F,093G,093H,093I,093J,093K,093L,093M,093N,093O,093P,094A,094B,094C,094D,094E,094F,094G,094H,094I,094J,094K,094L,094M,094N,094O,094P,102I,102O,102P,103A,103B,103C,103F,103G,103H,103I,103J,103K,103O,103P,104A,104B,104C,104F,104G,104H,104I,104J,104K,104L,104M,104N,104O,104P,114I,114O,114P"
@@ -37,15 +38,8 @@ def read_csv(path):
 def merge():
     """Merge output tile .gdb layers into single output
     """
-    click.echo("Merging tiles to output...")
     sources = read_csv(CONFIG["source_csv"])
     tiles = TILES.split(",")
-
-    out_gdb = os.path.join(os.getcwd(), "integrated_roads.gdb")
-
-    # remove existing output
-    if os.path.exists(out_gdb):
-        shutil.rmtree(out_gdb)
 
     # Build an orderd list of fields noted for retention
     out_fields = []
@@ -54,13 +48,13 @@ def merge():
         if source["alias"] != "tiles_20k" and source["preprocess_operation"] != "roadpoly2line":
             fieldlist = [source["primary_key"]] + source["fields"].split(",")
             # prepend the alias to the field name
-            out_fields = out_fields + [source["alias"] + "_" + f.lower() for f in fieldlist]
+            out_fields = out_fields + [source["alias"] + "_" + f.lower().strip() for f in fieldlist]
 
-    # build string remapping field name to UPPER
-    fieldstring = ", ".join([f.strip() + " AS " + f.strip().upper() for f in out_fields])
-
-    # loop through the tiles, appending to output file
+    db = pgdata.connect(CONFIG["db_url"], schema="public")
+    db["integrated_roads"].drop()
+    commands = []
     for tile in tiles:
+        click.echo("Examining tile {}".format(tile))
         in_gdb = os.path.join(CONFIG["temp_data"], "tiles", "temp_" + tile + ".gdb")
         in_layer = "roads_" + tile
 
@@ -76,38 +70,56 @@ def merge():
         if n_recs >= 1:
             # determine which fields are missing from the input
             missing_fields = list(set(out_fields).difference(set(in_fields)))
-            fieldquery = fieldstring
-            # substitute NULL as input for missing fields
-            for f in missing_fields:
-                fieldquery = fieldquery.replace(" "+f.lower()+" ", " NULL ")
-
+            # build list of fields to extract, substituting missing fields
+            # with NULL
+            query_fields = []
+            for i, f in enumerate(out_fields):
+                if f in missing_fields:
+                    query_fields.append("NULL AS " + f)
+                else:
+                    query_fields.append(f)
             sql = """SELECT
-                bcgw_source AS BCGW_SOURCE,
-                bcgw_extraction_date AS BCGW_EXTRACTION_DATE,
-                map_tile AS MAP_TILE_20K,
-                {f},
-                shape
+                bcgw_source,
+                bcgw_extraction_date,
+                map_tile,
+                {f}
                 FROM {lyr}""".format(
-                f=fieldquery, lyr=in_layer
+                f=", ".join(query_fields), lyr=in_layer
             )
-            command = [
+            commands.append([
                 "ogr2ogr",
                 "-f",
-                "FileGDB",
+                "PostgreSQL",
+                "PG:host={h} user={u} dbname={db} password={pwd}".format(
+                    h=db.host, u=db.user, db=db.database, pwd=db.password
+                ),
+                "-lco",
+                "SCHEMA={schema}".format(schema="public"),
+                "-lco",
+                "GEOMETRY_NAME=geom",
                 "-nln",
                 "integrated_roads",
                 "-dialect",
                 "SQLITE",
                 "-sql",
                 sql,
-                out_gdb,
-                in_gdb,
-            ]
-            if os.path.exists(out_gdb):
-                command.insert(1, "-update")
-                command.insert(2, "-append")
-            click.echo(" ".join(command))
-            subprocess.run(command)
+                in_gdb]
+            )
+    # execute the first command, remove it from the list
+    click.echo("Creating merged table by loading first tile")
+    click.echo(" ".join(commands[0]))
+    subprocess.run(commands[0])
+    commands.remove(commands[0])
+
+    click.echo("Loading remaining tiles...")
+    # add -update -append to the rest of the commands
+    for c in commands:
+        c.extend(["-update", "-append"])
+    # execute in parallel
+    # shuzhanfan.github.io/2017/12/parallel-processing-python-subprocess
+    procs_list = [Popen(cmd, stdout=PIPE, stderr=PIPE) for cmd in commands]
+    for proc in procs_list:
+        proc.wait()
 
 
 if __name__ == "__main__":
