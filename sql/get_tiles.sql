b279c65e91a655b1c4f8a3bfec26f89d71476708
Simon Norris <snorris@hillcrestgeo.ca>
2021-07-09 17:23:45 -0700

    remove unneeded file

--- sql/get_tiles.sql
+++
@@ -1,9 +0,0 @@
--- -----------------------
--- name: get_tiles
--- -----------------------
-
-SELECT 
-  map_tile
-FROM whse_basemapping.bcgs_20k_grid
-WHERE map_tile = '092C050'--LIKE '092C%%'
-LIMIT 1;
