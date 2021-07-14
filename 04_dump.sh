e8622ce4d3962615c770a7e87bd71b6705f21166
Simon Norris <snorris@hillcrestgeo.ca>
2021-07-13 17:43:25 -0700

    restore km per road source summary query in dump

--- 04_dump.sh
+++ 04_dump.sh
@@ -12,4 +12,18 @@ ogr2ogr \
     integratedroads.gpkg \
     "PG:host=$PGHOST user=$PGUSER dbname=$PGDATABASE port=$PGPORT"
 
+# summarize road source by length
+ogr2ogr \
+  -f GPKG \
+  -progress \
+  -update \
+  -nln bcgw_source_km \
+-sql "SELECT
+  bcgw_source,
+  round((sum(st_length(geom) / 1000)::numeric), 2)  as length_km
+FROM integratedroads_vw
+GROUP BY bcgw_source" \
+  integratedroads.gpkg \
+  "PG:host=localhost user=postgres dbname=roadintegrator password=postgres"
+
 zip -r integratedroads.gpkg.zip integratedroads.gpkg
