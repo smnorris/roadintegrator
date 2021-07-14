dc40c4e7ea5c3621383cbfd39f24949e4239af69
Simon Norris <snorris@hillcrestgeo.ca>
2021-07-13 17:09:43 -0700

    dump to gpkg as default

--- 04_dump.sh
+++ 04_dump.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Dump output to .gdb
-# This uses dockerized gdal for easy access to fgdb driver
-docker run --rm \
-  -v ${PWD}:/output \
-  osgeo/gdal:ubuntu-full-3.3.1-fgdb \
-  ogr2ogr \
-    -f FileGDB \
-    -progress \
-    -nlt LINESTRING \
-    -nln integrated_roads \
-    -lco GEOMETRY_NULLABLE=NO \
-    -lco GEOMETRY_NAME=Shape \
-    -sql "SELECT * FROM integratedroads_vw" \
-    integrated_roads.gdb \
-    "PG:host=$PGHOST user=$PGUSER dbname=$PGDATABASE port=$PGPORT"
-
+# dump to geopackage
 ogr2ogr \
     -f GPKG \
     -progress \
@@ -28,4 +13,3 @@ ogr2ogr \
     "PG:host=$PGHOST user=$PGUSER dbname=$PGDATABASE port=$PGPORT"
 
 zip -r integratedroads.gpkg.zip integratedroads.gpkg
-scp integratedroads.gpkg.zip snorris@hillcrestgeo.ca:/var/www/hillcrestgeo.ca/html/outgoing/public
--- misc/fgdb.sh
+++ misc/fgdb.sh
@@ -6,3 +6,18 @@ cd gdal/docker/ubuntu-full
 docker build --build-arg WITH_FILEGDB=yes --tag osgeo/gdal:ubuntu-full-3.3.1-fgdb .
 cd ../../../../
 rm -rf gdal
+
+# Dump output to .gdb
+docker run --rm \
+  -v ${PWD}:/output \
+  osgeo/gdal:ubuntu-full-3.3.1-fgdb \
+  ogr2ogr \
+    -f FileGDB \
+    -progress \
+    -nlt LINESTRING \
+    -nln integrated_roads \
+    -lco GEOMETRY_NULLABLE=NO \
+    -lco GEOMETRY_NAME=Shape \
+    -sql "SELECT * FROM integratedroads_vw" \
+    integrated_roads.gdb \
+    "PG:host=$PGHOST user=$PGUSER dbname=$PGDATABASE port=$PGPORT"
